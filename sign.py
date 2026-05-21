b5755758829f0d830b53b6df6e37d8497288a583
JimeWang <113106477+gutibJim@users.noreply.github.com>
2026-05-21 15:03:43 +0800

    Update sign.py

--- sign.py
+++ sign.py
@@ -2,6 +2,7 @@ import requests
 import os
 
 COOKIE = os.environ["COOKIE"]
+SENDKEY = os.environ["SENDKEY"]
 
 url = "https://www.ablesci.com/user/sign"
 
@@ -12,4 +13,16 @@ headers = {
 
 response = requests.get(url, headers=headers)
 
-print(response.text)
+data = response.json()
+
+msg = data["msg"]
+
+print(msg)
+
+# 微信通知
+push_url = f"https://sctapi.ftqq.com/{SENDKEY}.send"
+
+requests.post(push_url, data={
+    "title": "AbleSci 自动签到",
+    "desp": msg
+})
